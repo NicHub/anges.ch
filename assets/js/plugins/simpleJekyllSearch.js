c2e38af8e26d5692bce63660a259a23cad9740b1
NicHub <nicolas.jeanmonod@gmail.com>
2024-01-04 15:29:19 +0100

    w3c compliance

--- assets/js/plugins/simpleJekyllSearch.js
+++ assets/js/plugins/simpleJekyllSearch.js
@@ -13,7 +13,7 @@
             searchResults       : '.results',
             searchResultsTitle  : '<h4>Search results</h4>',
             limit               : '10',
-            noResults           : '<p>Oh shucks<br/><small>Nothing found :(</small></p>'
+            noResults           : '<p>Oh shucks<br><small>Nothing found :(</small></p>'
         }, options);
 
         var jsonData = [],
@@ -46,7 +46,7 @@
                 if(e.which === 13){
                     if(matches)
                         window.location = matches[0].url;
-                        
+
                     //follow the first link
                     // if(searchResults.children().length)
                 }
